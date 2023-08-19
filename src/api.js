52d5f06daf343741ece99dad4999298255224e94
ToTrinhBui <totriinh@gmail.com>
2023-08-19 19:17:58 +0700

    Config URL API

--- src/api.js
+++ src/api.js
@@ -1,1 +1,2 @@
-export const URL_API = 'http://localhost:3001';
+// export const URL_API = 'http://localhost:3001';
+export const URL_API = 'https://my-json-server.typicode.com/ToTrinhBui/trello-clone';
