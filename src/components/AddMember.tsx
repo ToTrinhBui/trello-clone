685bbc57afb45305a6902f953f21d4c1c59b5f87
ToTrinhBui <totriinh@gmail.com>
2023-10-19 15:29:27 +0700

    edit avatar user

--- src/components/AddMember.tsx
+++ src/components/AddMember.tsx
@@ -112,7 +112,7 @@ const AddMember: React.FC<AddMemberProps> = ({ members, owner, memberFilter }) =
                 <p>Chia sẻ</p>
             </button>
             <Dialog onClose={handleToClose} open={open} className='dialog'>
-                <div className='dialog-container' style={{ width: '550px' }}>
+                <div className='dialog-container add-mem' style={{ width: '550px' }}>
                     <div className='title-box'>
                         <h3>Chia sẻ bảng</h3>
                         <div className='close btn' onClick={handleToClose}>
@@ -155,7 +155,9 @@ const AddMember: React.FC<AddMemberProps> = ({ members, owner, memberFilter }) =
                     <div className='list-users-board'>
                         <div className='user-in4'>
                             <div className='user-email'>
-                                <img alt='avatar' style={{ width: '35px' }} src='https://trello-members.s3.amazonaws.com/64a23b00afb58bcc432fbd06/c080abc7f45703f2f7b91e88d2e41202/50.png' />
+                                <div className="avatar" style={{ height: '35px', width: '35px' }}>
+                                    <svg fill="#61ae74" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="796 796 200 200" enableBackground="new 796 796 200 200" stroke="#61ae74"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M896,796c-55.14,0-99.999,44.86-99.999,100c0,55.141,44.859,100,99.999,100c55.141,0,99.999-44.859,99.999-100 C995.999,840.86,951.141,796,896,796z M896.639,827.425c20.538,0,37.189,19.66,37.189,43.921c0,24.257-16.651,43.924-37.189,43.924 s-37.187-19.667-37.187-43.924C859.452,847.085,876.101,827.425,896.639,827.425z M896,983.86 c-24.692,0-47.038-10.239-63.016-26.695c-2.266-2.335-2.984-5.775-1.84-8.82c5.47-14.556,15.718-26.762,28.817-34.761 c2.828-1.728,6.449-1.393,8.91,0.828c7.706,6.958,17.316,11.114,27.767,11.114c10.249,0,19.69-4.001,27.318-10.719 c2.488-2.191,6.128-2.479,8.932-0.711c12.697,8.004,22.618,20.005,27.967,34.253c1.144,3.047,0.425,6.482-1.842,8.817 C943.037,973.621,920.691,983.86,896,983.86z"></path> </g></svg>
+                                </div>
                                 <p>{user_redux.email}</p>
                             </div>
                             {owner === user_redux.id ?
--- src/components/ListMember.tsx
+++ src/components/ListMember.tsx
@@ -45,7 +45,7 @@ const ListMember: React.FC<CardProps> = ({ close }) => {
 
     return ReactDOM.createPortal(
         <div className='overlay-list-member' id={theme}>
-            <div className='card-box' style={{width: '225px', maxHeight: '300px', overflowX: 'auto'}}>
+            <div className='card-box' style={{ width: '225px', maxHeight: '300px', overflowX: 'auto' }}>
                 <div className='title-card'>
                     <h4 style={{ width: '90%', textAlign: 'center' }}>Thành viên của Trello</h4>
                     <div className='close-card btn' onClick={close}>
@@ -65,7 +65,9 @@ const ListMember: React.FC<CardProps> = ({ close }) => {
                 </div>
                 <div className='card-content'>
                     <div className='member-outer'>
-                        <div className="avatar" style={{ height: '30px', width: '30px' }}></div>
+                        <div className="avatar" style={{ height: '30px', width: '30px' }}>
+                            <svg fill="#61ae74" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="796 796 200 200" enableBackground="new 796 796 200 200" stroke="#61ae74"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M896,796c-55.14,0-99.999,44.86-99.999,100c0,55.141,44.859,100,99.999,100c55.141,0,99.999-44.859,99.999-100 C995.999,840.86,951.141,796,896,796z M896.639,827.425c20.538,0,37.189,19.66,37.189,43.921c0,24.257-16.651,43.924-37.189,43.924 s-37.187-19.667-37.187-43.924C859.452,847.085,876.101,827.425,896.639,827.425z M896,983.86 c-24.692,0-47.038-10.239-63.016-26.695c-2.266-2.335-2.984-5.775-1.84-8.82c5.47-14.556,15.718-26.762,28.817-34.761 c2.828-1.728,6.449-1.393,8.91,0.828c7.706,6.958,17.316,11.114,27.767,11.114c10.249,0,19.69-4.001,27.318-10.719 c2.488-2.191,6.128-2.479,8.932-0.711c12.697,8.004,22.618,20.005,27.967,34.253c1.144,3.047,0.425,6.482-1.842,8.817 C943.037,973.621,920.691,983.86,896,983.86z"></path> </g></svg>
+                        </div>
                         <div>{user_redux.email}</div>
                     </div>
                     {members?.map((member, index) => (
